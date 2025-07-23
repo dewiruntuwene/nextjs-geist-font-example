b2725458fa292cf18cfaa6213857aa7668a50c30
Dewi Fareley <71383399+dewiruntuwene@users.noreply.github.com>
2025-07-23 13:21:33 +0700

    Add .next/static/chunks/src_app_assistant_page_tsx_a993527f._.js 1753251692511

---
+++ .next/static/chunks/src_app_assistant_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_4c5b78ed._.js",
+  "static/chunks/node_modules_48874fa9._.js"
+],
+    source: "dynamic"
+});
